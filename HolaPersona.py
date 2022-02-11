be80870c5549b76f3a353c7d1f156e66d653f5c2
Axel Dardon <axeldardon@gmail.com>
2022-02-10 18:23:28 -0600

    pruebapy

--- HolaPersona.py
+++ HolaPersona.py
@@ -1,5 +1,3 @@
-import datetime
-
-
+from datetime import datetime
 NombrePersona = input("Por favor ingrese su nombre: ")
 print("Hola un saludo", NombrePersona," en la fecha:",datetime.today().strftime("%Y-%m-%d"))
